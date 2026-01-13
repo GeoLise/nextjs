8e1a947bba52ab22922a53b7fae60b262fa7735a
GeoLise <jora.liseenko@yandex.ru>
2026-01-14 01:39:54 +0300

    start

---
+++ src/app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <div className="bg-foreground flex md:flex-row flex-col md:text-white text-green-400 md:text-9xl text-xl md:gap-10 gap-4">
+      <p>Products</p>
+      <p>catalog</p>
+    </div>
+  );
+}
